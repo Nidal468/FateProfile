40569ea5b9ab098056c0a39161c3af21e3debe9e
Fate 468 <115021956+Nidal468@users.noreply.github.com>
2023-01-23 07:05:34 +0000

    version 1.3

--- pages/components/basic.js
+++ pages/components/basic.js
@@ -1,10 +1,10 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import Link from 'next/link';
 
 import nav from '/styles/Nav.module.css'
 import Data from '/data/basic.json';
 
-export function Nav() {
+export function Nav(props) {
 	const [invisible, setInvisible] = useState(false);
 	const [isMenu, setMenu] = useState(false)
 	
@@ -12,15 +12,15 @@ export function Nav() {
 		setMenu(current =>! current);
 	};
    return(
-		 <nav className={nav.nav_container}>
+		 <nav className={nav.nav_container} style={{borderBottom:props.border, color:props.color}}>
 		 <h3>{Data[0].name}</h3>
 			 <div className={nav.nav_options}>
-			 <a href="#">Home</a>
-			 <a href="#">About</a>
-			 <a href="#">Designs</a>
-			 <a href="#">Projects</a>
-			 <a href="#">Gigs</a>
-			 <a><Link href="/games">Games</Link></a>
+			 <Link href="/">Home</Link>
+			 <Link href="#">About</Link>
+			 <Link href="#">Designs</Link>
+			 <Link href="#">Projects</Link>
+			 <Link href="#">Gigs</Link>
+			 <Link href="/games">Games</Link>
 			 </div>
 			 <div className={nav.search}>
 				<i className="fi fi-rr-search" onClick={() => setInvisible(current =>! current)} style={{"display":invisible? "none":"block"}}></i>
--- pages/games.tsx
+++ pages/games.tsx
@@ -1,26 +1,33 @@
 import type { NextPage } from 'next'
 
 import {Nav} from './components/basic.js';
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, EffectCreative} from "swiper";
 
 import games from '../styles/games.module.css'
+
+import tofs from '../data/games.json';
 
 const Games: NextPage = () => {
    return( 
 			 <div className={games.main}>
-				 <Nav/>
+				 <Nav border="1px solid #fff"/>
 				 <div className={games.body}>
-				 <div className={games.banner}></div>
+				 <img src="images/tof.jpg" className={games.banner}/>
+					 <h2>Check out stuffs that I have made that aren't related to work</h2>
 					 <div className={games.box_container}>
-				 <div className={games.box}>
-					 <img src=""/>
+						  {tofs.map(tof => {
+				return(
+					<div className={games.box}> 
+					 <img src={tof.img}/>
 				 <div className={games.teraria}>
-					 <h3>Teraria</h3>
-					 <p style={{fontSize:"8px", bottom:"20px"}}>In order to play on our server first you need to download the same version of Teraria that the server has then you need to download Zero Tier from your IOS or Play store. Open your Zero Tier then paste the code below to connect to the server then open teraria and paste the server ID then play.</p>
-					 <p><br/><a>Download Teraria</a><br/><a href="https://play.google.com/store/apps/details?id=com.zerotier.one">Download Zero Tier</a><br/>Zero Tier :8286ac0e4719038f<br/>Server id :172.25.142.73</p>
+					 <h3>{tof.title}</h3>
+					 <p style={{fontSize:"8px", bottom:"20px"}}>{tof.info}</p>
+					 <p><br/><a href={tof.link}>{tof.link_name}</a><br/><a href={tof.link1}>{tof.link_name1}</a><br/>{tof.address}<br/>{tof.id}</p>
 				 </div>
+					
 				 </div>
+							)
+			 })};
+				 
 					 </div>
 				 </div>
 			 </div>
