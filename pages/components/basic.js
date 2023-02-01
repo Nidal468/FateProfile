4c9a0bbd5c2d63ea04725d5fd1af0c80206509a1
Fate 468 <115021956+Nidal468@users.noreply.github.com>
2023-02-01 13:20:18 +0000

    version 1.8

--- pages/components/basic.js
+++ pages/components/basic.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import nav from '/styles/Nav.module.css'
 import Data from '/data/basic.json';
+import TextField  from '@mui/material/TextField';
 
 export function Nav(props) {
 	const [invisible, setInvisible] = useState(false);
@@ -12,7 +13,7 @@ export function Nav(props) {
 		setMenu(current =>! current);
 	};
    return(
-		 <nav className={nav.nav_container} style={{borderBottom:props.border, color:props.color}}>
+		 <nav className={nav.nav_container} style={{borderBottom:props.border, color:props.color, position:props.position}}>
 		 <h3>{Data[0].name}</h3>
 			 <div className={nav.nav_options}>
 			 <Link href="/">Home</Link>
@@ -24,7 +25,7 @@ export function Nav(props) {
 			 </div>
 			 <div className={nav.search}>
 				<i className="fi fi-rr-search" onClick={() => setInvisible(current =>! current)} style={{"display":invisible? "none":"block"}}></i>
-				<input type="text" placeholder="search" style={{"width":invisible? "100%":"0","opacity":invisible? "100%":"0"}} />
+				<TextField id="outlined-basic" label="Search" variant="outlined" style={{"width":invisible? "100%":"0","opacity":invisible? "100%":"0"}} className={nav.textfield}/>
 						<i className="fi fi-rr-angle-right" onClick={() => setInvisible(current =>! current)} style={{"display":invisible? "block":"none"}}></i>
 					</div>
 			 <div className={isMenu? nav.close:""}>
--- pages/url/soft_landing_page.tsx
+++ pages/url/soft_landing_page.tsx
@@ -1,38 +1,54 @@
 import type{NextPage} from 'next';
-
 import {Nav} from '../components/basic.js';
-import Image from 'next/image';
-
-import page from '../data/page.json';
+import Button from '@mui/material/Button';
 
 import soft from '/styles/soft_landing_page.module.css'
 
 const softs: NextPage = () =>{
 	return(
 		<div className={soft.main}>
-			<Nav/>
+			<Nav border="1px solid #fff" position="absolute"/>
 			<div className={soft.hero}>
-				<svg id="svg" viewBox="300 0 1440 590" xmlns="http://www.w3.org/2000/svg">
-					<defs>
-						<linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
-							<stop offset="5%" stop-color="#F78DA7"></stop>
-							<stop offset="95%" stop-color="#8ED1FC"></stop>
-						</linearGradient>
-					</defs>
-					<path d="M 0,600 C 0,600 0,150 0,150 C 71.16746411483251,120.99521531100478 142.33492822966502,91.99043062200958 253,97 C 363.665071770335,102.00956937799042 513.8277511961722,141.03349282296648 621,167 C 728.1722488038278,192.96650717703352 792.3540669856459,205.8755980861244 885,204 C 977.6459330143541,202.1244019138756 1098.7559808612439,185.4641148325359 1196,174 C 1293.2440191387561,162.5358851674641 1366.622009569378,156.26794258373207 1440,150 C 1440,150 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="0.4" className={soft.path0} transform="rotate(-180 720 300)"></path>
-					<defs>
-						<linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%"><stop offset="5%" stop-color="#F78DA7"></stop><stop offset="95%" stop-color="#8ED1FC"></stop>
-						</linearGradient>
-					</defs>
-					<path d="M 0,600 C 0,600 0,300 0,300 C 88.2870813397129,286.9569377990431 176.5741626794258,273.9138755980861 273,290 C 369.4258373205742,306.0861244019139 473.9904306220096,351.30143540669854 574,351 C 674.0095693779904,350.69856459330146 769.4641148325359,304.88038277511953 867,280 C 964.5358851674641,255.11961722488044 1064.153110047847,251.177033492823 1160,258 C 1255.846889952153,264.822966507177 1347.9234449760766,282.4114832535885 1440,300 C 1440,300 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="0.53" className={soft.path1} transform="rotate(-180 720 300)"></path>
-					<defs>
-						<linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
-							<stop offset="5%" stop-color="#F78DA7"></stop>
-							<stop offset="95%" stop-color="#8ED1FC"></stop>
-						</linearGradient>
-					</defs>
-					<path d="M 0,600 C 0,600 0,450 0,450 C 126.59330143540669,432.43062200956933 253.18660287081337,414.8612440191387 337,431 C 420.8133971291866,447.1387559808613 461.8468899521531,496.9856459330144 552,495 C 642.1531100478469,493.0143540669856 781.4258373205741,439.19617224880386 895,417 C 1008.5741626794259,394.80382775119614 1096.4497607655503,404.2296650717704 1183,415 C 1269.5502392344497,425.7703349282296 1354.7751196172248,437.88516746411483 1440,450 C 1440,450 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="1" className={soft.path2} transform="rotate(-180 720 300)"></path></svg>
-		</div>
+				<div>
+				<h1>I will Build you a website for you or your business</h1>
+				<p>My websites are Fast, Responsive, Modern and unique so users will have a pleasant experience.</p>
+					<a href="#">Order Now<i className="fi fi-rr-caret-right"></i></a>
+				</div>
+						<img src="/images/dark_hero.png"/>
+			</div>
+			<svg id="svg" xmlns="http://www.w3.org/2000/svg">
+					<path d="M 0,400 C 0,400 0,200 0,200 C 102.07655502392345,174.39234449760767 204.1531100478469,148.7846889952153 298,157 C 391.8468899521531,165.2153110047847 477.46411483253587,207.25358851674642 583,209 C 688.5358851674641,210.74641148325358 813.9904306220096,172.20095693779905 898,167 C 982.0095693779904,161.79904306220095 1024.5741626794259,189.94258373205741 1108,201 C 1191.4258373205741,212.05741626794259 1315.712918660287,206.0287081339713 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" strokeWidth="0" fill="#3B30EC" fillOpacity="1" className={soft.path0} transform="rotate(-180 720 200)"></path>
+				</svg>
+			<h2>My top 3 Gigs</h2>
+			<div className={soft.gig_container}>
+				<div className={soft.gig}>
+					<img src="/images/fiverr-logo.png"/>
+					<div className={soft.gig_info}>
+						<h3>I will build you a Next js website </h3>
+						<p>I will design a responsive website for you be it a html, css and java script website or a React/Next JS website.</p>
+					</div>
+					<Button variant="contained">View</Button>
+				</div>
+				<div className={soft.gig}>
+					<img src="/images/fiverr-logo.png"/>
+					<div className={soft.gig_info}>
+						<h3>I will build you a Next js website </h3>
+						<p>I will design a responsive website for you be it a html, css and java script website or a React/Next JS website.</p>
+					</div>
+					<Button variant="contained">View</Button>
+				</div>
+				<div className={soft.gig}>
+					<img src="/images/fiverr-logo.png"/>
+					<div className={soft.gig_info}>
+						<h3>I will build you a Next js website </h3>
+						<p>I will design a responsive website for you be it a html, css and java script website or a React/Next JS website.</p>
+					</div>
+					<Button variant="contained">View</Button>
+				</div>
+			</div>
+			<div className={soft.gig_list}>
+				<h2></h2>
+			</div>
 		</div>
 	)
 };
